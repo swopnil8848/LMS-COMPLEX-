e4e101fe85ab7737684c382f97559cfc5e18cece
swopnil <108117980+swopnil8848@users.noreply.github.com>
2025-06-18 19:51:54 +0545

    minor fixes

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -2,13 +2,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import type { AppDispatch, RootState } from '../app/store'; // adjust path to your store
 import { useEffect } from 'react';
+import { getMe } from '../features/auth/authSlice';
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const dispatch = useDispatch<AppDispatch>();
   const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
 
   useEffect(()=>{
-    dis
+    dispatch(getMe());
   })
 
   return isAuthenticated ? children : <Navigate to="/login" />;
