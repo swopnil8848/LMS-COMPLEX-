e88da1ac0725c3d0726b2afc528aceb947d6641d
swopnil <108117980+swopnil8848@users.noreply.github.com>
2025-06-18 20:23:52 +0545

    shwoing exaples

--- src/pages/dashboard/DashboardHome.tsx
+++ src/pages/dashboard/DashboardHome.tsx
@@ -1,5 +1,14 @@
+import type { RootState } from "../../app/store";
+import { useSelector } from "react-redux";
 
 const DashboardHome = () => {
+  //example of how to get data in page (yeman)
+  const { isAuthenticated, loading,user } = useSelector((state: RootState) => state.auth);
+
+  // example (yeman)
+  console.log("isAuthenticated:: ",isAuthenticated);
+  console.log("user:: ",user)
+
   return (
     <div>DashboardHome</div>
   )
